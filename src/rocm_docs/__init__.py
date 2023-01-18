41c13835f9cd5829659a97f23a26dff1d2fe66c8
Lauren Wrubleski <Lauren.Wrubleski@amd.com>
2023-01-18 12:26:09 -0700

    Initial commit

---
+++ src/rocm_docs/__init__.py
@@ -0,0 +1,179 @@
+"""Set up variables for documentation of ROCm projects using RTD."""
+import os
+import shutil
+import sys
+from pathlib import Path
+from typing import BinaryIO, Union
+from .util import format_toc
+
+
+if sys.version_info < (3, 9):
+    # importlib.resources either doesn't exist or lacks the files()
+    # function, so use the PyPI version:
+    import importlib_resources
+
+else:
+    # importlib.resources has files(), so use that:
+    import importlib.resources as importlib_resources
+
+
+def setup_rocm_docs(
+    project_name: str, repository_url: str, docs_folder: Union[str, os.PathLike, None] = None
+):
+    """Sets up default RTD variables and copies necessary files.
+
+    Args:
+        project_name (str): The name of the project.
+        docs_folder (str | os.PathLike, optional): The path to the            \
+            documentation folder. Defaults to None.
+
+    Raises:
+        NotADirectoryError: The specified documentation folder does not exist \
+            or cannot be read.
+        FileNotFoundError: The input table of contents file does not exist or \
+            cannot be read.
+        NotADirectoryError: An error occurred when trying to copy files from  \
+            the rocm_docs package.
+    """
+    # pylint: disable=global-variable-undefined, invalid-name
+    global copyright
+    global author
+    global project
+    # pylint: disable=redefined-builtin
+    copyright = "2022-2023, Advanced Micro Devices Ltd."
+    # pylint: enable=redefined-builtin
+    author = (
+        'Advanced Micro Devices <a href="https://">Disclaimer and Licensing'
+        " Info</a>"
+    )
+    project = project_name
+
+    # -- General configuration ------------------------------------------------
+    # -- General configuration
+    global extensions
+    extensions = [
+        "sphinx.ext.duration",
+        "sphinx.ext.doctest",
+        "sphinx.ext.autodoc",
+        "sphinx.ext.autosummary",
+        "sphinx.ext.intersphinx",
+        "sphinx_external_toc",
+        "sphinx_design",
+        "sphinx_copybutton",
+        "myst_nb",
+    ]
+
+    # MyST Configuration
+    global myst_enable_extensions
+    global myst_heading_anchors
+    myst_enable_extensions = ["colon_fence", "linkify"]
+    myst_heading_anchors = 3
+
+    # Table of Contents Configuration
+    if docs_folder is None:
+        docs_folder = Path.cwd()
+    elif not isinstance(docs_folder, Path):
+        docs_folder = Path(docs_folder)
+    if not (docs_folder.exists() and docs_folder.is_dir()):
+        raise NotADirectoryError(
+            f"Expected docs folder {docs_folder} to exist and be readable."
+        )
+
+    toc_in_path = docs_folder / "_toc.yml.in"
+    if not (toc_in_path.exists() and toc_in_path.is_file()):
+        raise FileNotFoundError(
+            f"Expected input toc file {toc_in_path} to exist and be readable."
+        )
+    url, branch = format_toc(docs_folder)
+
+    global external_toc_path
+    global external_toc_exclude_missing
+    external_toc_path = "_toc.yml"
+    external_toc_exclude_missing = False
+
+    # intersphinx Configuration
+    global intersphinx_mapping
+    global intersphinx_disabled_domains
+    intersphinx_mapping = {
+        "rtd": ("https://docs.readthedocs.io/en/stable/", None),
+        "python": ("https://docs.python.org/3/", None),
+        "sphinx": ("https://www.sphinx-doc.org/en/master/", None),
+    }
+    intersphinx_disabled_domains = ["std"]
+
+    # Other configuration
+    global templates_path
+    global epub_show_urls
+    global exclude_patterns
+    templates_path = ["_templates"]
+
+    # -- Options for EPUB output
+    epub_show_urls = "footnote"
+
+    # List of patterns, relative to source directory, that match files and
+    # directories to ignore when looking for source files.
+    # This pattern also affects html_static_path and html_extra_path.
+    exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+    # -- Options for HTML output ----------------------------------------------
+    global html_theme
+    global html_title
+    global html_static_path
+    global html_css_files
+    global html_js_files
+    global html_extra_path
+    global html_theme_options
+    global html_show_sphinx
+    global html_favicon
+
+    # The theme to use for HTML and HTML Help pages.  See the documentation for
+    # a list of builtin themes.
+    #
+    html_theme = "sphinx_book_theme"
+    html_title = project
+    html_static_path = ["_static"]
+    html_css_files = ["custom.css", "rocm_header.css", "rocm_footer.css"]
+    html_js_files = ["code_word_breaks.js"]
+    html_extra_path = ["_images"]
+    html_theme_options = {
+        "home_page_in_toc": True,
+        "use_edit_page_button": True,
+        "repository_url": url,
+        "repository_branch": branch,
+        "path_to_docs": "docs",
+        "show_navbar_depth": "2",
+        "body_max_width": "none",
+        "show_toc_level": "0",
+        "extra_navbar": "",
+    }
+
+    html_show_sphinx = False
+    html_favicon = "https://www.amd.com/themes/custom/amd/favicon.ico"
+
+    # Insert additional files into workspace
+    def copy_from_package(data_dir, read_path: str, write_path: str):
+        if not data_dir.is_dir():
+            raise NotADirectoryError(
+                f"Expected path {read_path}/{data_dir} to be traversable."
+            )
+        for entry in data_dir.iterdir():
+            entry_path: Path = docs_folder / write_path / entry.name
+            if entry.is_dir():
+                entry_path.mkdir(parents=True, exist_ok=True)
+                copy_from_package(
+                    entry,
+                    read_path + "/" + entry.name,
+                    write_path + "/" + entry.name,
+                )
+            else:
+                with entry.open_binary() as resource_file:
+                    # We open the resource file as a binary stream instead of a
+                    # file on disk because the latter may require unzipping
+                    # and/or the creation of a temporary file. This is not the
+                    # case when opening the file as a stream.
+                    resource_file: BinaryIO
+                    with open(entry_path, "wb") as output_file:
+                        shutil.copyfileobj(resource_file, output_file)
+
+    pkg = importlib_resources.files("rocm_docs")
+    copy_from_package(pkg / "data", "data", ".")
---
+++ src/rocm_docs/util.py
@@ -0,0 +1,61 @@
+import os
+import re
+from typing import Optional, Union
+from git import Repo, Remote, RemoteReference
+from pathlib import Path
+
+
+def get_branch(repo_path: Union[str, os.PathLike, None] = None):
+    git_url = re.compile(r'git@(\w+(?:\.\w+)+):(.*)\.git')
+    if repo_path is None:
+        repo_path = Path()
+    elif not isinstance(repo_path, Path):
+        repo_path = Path(repo_path)
+    repo = Repo(repo_path, search_parent_directories=True)
+    assert not repo.bare
+    for branch in repo.branches:
+        if branch.commit == repo.head.commit:
+            tracking = branch.tracking_branch()
+            if tracking is not None:
+                remote_url = repo.remotes[tracking.remote_name].url
+                remote_url = git_url.sub('http://\\1/\\2', remote_url)
+                return remote_url, tracking.remote_head
+    for remote in repo.remotes:
+        remote: Remote
+        for ref in remote.refs:
+            ref: RemoteReference
+            if ref.commit == repo.head.commit:
+                remote_url = git_url.sub('http://\\1/\\2', remote.url)
+                return remote_url, ref.remote_head
+    raise TypeError("Could not find the commit in the git repo.")
+
+
+def format_toc(
+    toc_path: Union[str, os.PathLike, None] = None,
+    repo_path: Union[str, os.PathLike, None] = None,
+    input_name: Optional[str] = None,
+    output_name: Optional[str] = None,
+):
+    if toc_path is None:
+        toc_path = Path()
+    elif not isinstance(toc_path, Path):
+        toc_path = Path(toc_path)
+    if repo_path is None:
+        repo_path = toc_path
+    input_name = "_toc.yml.in" if input_name is None else input_name
+    output_name = "_toc.yml" if output_name is None else output_name
+    at_start = True
+
+    url, branch = get_branch(repo_path)
+    with open(toc_path / input_name, "r", encoding="utf-8") as input:
+        with open(toc_path / output_name, "w", encoding="utf-8") as output:
+            for line in input.readlines():
+                if line[0] == "#" and at_start:
+                    continue
+                at_start = False
+                output.write(line.format(branch=branch, url=url))
+    return url, branch
+
+
+if __name__ == "__main__":
+    format_toc()
