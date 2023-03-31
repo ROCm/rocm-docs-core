4789f92d8a4a2e0d9fc0d8aeb4b1337079bbd8a7
Gergely Meszaros <gergely@streamhpc.com>
2023-03-31 16:59:37 +0000

    Make theme settings overrideable
    Respect any user defined settings and only provide defaults.
    - Move simple defaults to theme.conf, user settings override it by default
    - And prefer the users value for the more complex ones

--- src/rocm_docs/theme.py
+++ src/rocm_docs/theme.py
@@ -6,33 +6,52 @@ from pathlib import Path
 from pydata_sphinx_theme.utils import get_theme_options_dict
 import rocm_docs.util as util
 
-def _update_theme_options(app: Sphinx) -> None:
-    url, branch, edit_page = util.get_branch(app.srcdir)
 
-    options = get_theme_options_dict(app)
-    options.update({
-        "home_page_in_toc": False,
+def _update_edit_opts(
+    srcdir: Path, theme_opts: Dict[str, Any]
+) -> Dict[str, Any]:
+    # Bail if the user explicitly disabled the edit button
+    if not theme_opts.get("use_edit_page_button", True):
+        return
+
+    url, branch, edit_page = util.get_branch(srcdir)
+    default_branch_options = {
         "use_edit_page_button": edit_page,
         "repository_url": url,
         "repository_branch": branch,
-        "path_to_docs": util.get_path_to_docs(app.srcdir),
-        "show_navbar_depth": "0",
-        "body_max_width": "none",
-        "show_toc_level": "0",
-        "article_header_start": [
-            "toggle-primary-sidebar.html",
-            "breadcrumbs.html",
-        ],
-    })
-    if options.get("link_main_doc", True):
-        options["navbar_center"] = ["components/left-side-menu"]
+        "path_to_docs": util.get_path_to_docs(srcdir),
+    }
+    for k, v in default_branch_options.items():
+        theme_opts.setdefault(k, v)
+
+
+def _update_theme_options(app: Sphinx) -> None:
+    theme_opts = get_theme_options_dict(app)
+    _update_edit_opts(app.srcdir, theme_opts)
+
+    theme_opts.setdefault(
+        "article_header_start",
+        ["toggle-primary-sidebar.html", "breadcrumbs.html"],
+    )
+
+    # Prepend or set as value if the list doesn't yet exist
+    if theme_opts.get("link_main_doc", True):
+        theme_opts.setdefault("navbar_center", []).insert(
+            0, "components/left-side-menu"
+        )
+
 
 def setup(app: Sphinx) -> Dict[str, Any]:
     app.add_js_file("code_word_breaks.js", loading_method="async")
     here = Path(__file__).parent.resolve()
     theme_path = here / "rocm_docs_theme"
     app.add_html_theme("rocm_docs_theme", str(theme_path))
-    for css in ["custom.css", "rocm_header.css", "rocm_footer.css", "fonts.css"]:
+    for css in [
+        "custom.css",
+        "rocm_header.css",
+        "rocm_footer.css",
+        "fonts.css",
+    ]:
         app.add_css_file(css)
     app.connect("builder-inited", _update_theme_options)
 
