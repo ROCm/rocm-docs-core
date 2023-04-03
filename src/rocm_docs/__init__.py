52312010a645d5c533bc4e22c97fbfe5de76682d
Gergely Meszaros <gergely@streamhpc.com>
2023-04-03 15:07:05 +0000

    feat: move automatic doxygen and doxysphinx to an extension
    This finalizes making all functionality into a sphinx extension.
    Using it via this new interface looks like:
    ```python3
    extensions=["rocm_docs.doxygen"]
    doxygen_root="<path to folder where doxygen should be executed>"
    doxyfile="<path to doxygen configuration file>"
    doxygen_project={"name":<project-name>, "path":<xml-output-path>}
    doxysphinx_enabled=<True|False>
    ```
    
    The ROCmDocs interface is still preserved, but it only sets these
    variables now, it can be deprecated later.

--- src/rocm_docs/__init__.py
+++ src/rocm_docs/__init__.py
@@ -1,24 +1,9 @@
 """Set up variables for documentation of ROCm projects using RTD."""
 import os
-import shutil
-import subprocess
-import sys
-from dataclasses import dataclass
 from pathlib import Path
-from typing import BinaryIO, List, Optional, Union, Dict
-from sphinx.application import Sphinx
+from typing import BinaryIO, Dict, Generator, List, Optional, Union
 
-import rocm_docs.doxygen as doxygen
 from rocm_docs.core import setup
-
-if sys.version_info < (3, 9):
-    # importlib.resources either doesn't exist or lacks the files()
-    # function, so use the PyPI version:
-    import importlib_resources
-
-else:
-    # importlib.resources has files(), so use that:
-    import importlib.resources as importlib_resources
 
 MaybePath = Union[str, os.PathLike, None]
 
@@ -31,46 +16,32 @@ class ROCmDocs:
 
     SPHINX_VARS = [
         "extensions",
-        "breathe_projects",
-        "breathe_default_project",
         "html_title",
         "html_theme_options",
+        "doxygen_root",
+        "doxygen_project",
+        "doxyfile",
+        "doxysphinx_enabled",
     ]
 
     def __init__(
         self,
         project_name: str,
-        docs_folder: MaybePath = None,
+        _: MaybePath = None,
     ) -> None:
         self._project_name = project_name
-        self.extensions: List[str]
-        self.breathe_projects: Dict[str, str]
-        self.breathe_default_project: str
+        self.extensions: List[str] = []
         self.html_title: str
         self.html_theme_options: Dict[str, Union[str, bool, List[str]]] = {}
-        self._docs_folder: Path
-        tmp_docs_folder = self.to_path(docs_folder)
-        self._docs_folder = (
-            tmp_docs_folder if tmp_docs_folder is not None else Path(".")
-        )
-        self._doxygen_context = doxygen._DoxygenContext()
-        self._copied_files = False
-        self._ran_doxygen = False
-        self._setup = False
+        self.doxygen_root: MaybePath
+        self.doxygen_project: Tuple[Optional[str], MaybePath]
+        self.doxyfile: MaybePath
+        self.doxysphinx_enabled = False
 
     @property
     def project(self) -> str:
         """Sphinx project variable."""
         return self._project_name
-
-    @staticmethod
-    def to_path(path_obj: MaybePath) -> Optional[Path]:
-        """Ensure that an object is a pathlib Path."""
-        if path_obj is None:
-            return path_obj
-        if not isinstance(path_obj, Path):
-            return Path(path_obj)
-        return path_obj
 
     def run_doxygen(
         self,
@@ -78,149 +49,30 @@ class ROCmDocs:
         doxygen_path: MaybePath = None,
         doxygen_file: Optional[str] = None,
     ) -> None:
-        """Run doxygen, validating all paths."""
-        if self._ran_doxygen:
-            # Only run doxygen once
-            return
-        self._ran_doxygen = True
-        doxygen_root = self.to_path(doxygen_root)
-        if doxygen_root is None:
-            doxygen_root = Path("./.doxygen")
-        doxygen_path = self.to_path(doxygen_path)
-        if doxygen_path is None:
-            doxygen_path = Path("docBin/xml")
-        doxygen_file = "Doxyfile" if doxygen_file is None else doxygen_file
+        if not "rocm_docs.doxygen" in self.extensions:
+            self.extensions.append("rocm_docs.doxygen")
 
-        try:
-            doxygen_root = doxygen_root.resolve(strict=True)
-        except FileNotFoundError as err:
-            raise NotADirectoryError(
-                f"Expected doxygen root folder {doxygen_root} to exist"
-                " and be readable."
-            ) from err
-        doxyfile = doxygen_root / doxygen_file
-        if not doxyfile.is_file():
-            raise FileNotFoundError(
-                f"Expected doxyfile {doxyfile} to exist and be readable."
-            )
-
-        doxygen_exe = shutil.which("doxygen")
-        if doxygen_exe is None:
-            raise RuntimeError("'doxygen' command not found! Make sure that "
-                               "doxygen is installed and in the PATH")
-
-        
-        # Running doxygen requires that the files are already copied because 
-        # the Doxyfile references files distributed with rocm-docs-core
-        # (e.g. stylesheets)
-        self.copy_files()
-        try:
-            subprocess.check_call([doxygen_exe, "--version"], cwd=doxygen_root)
-            subprocess.check_call([doxygen_exe, doxygen_file], cwd=doxygen_root)
-        except subprocess.CalledProcessError as err:
-            raise RuntimeError("Failed when running doxygen") from err
-        try:
-            doxygen_path = doxygen_path.resolve(strict=True)
-        except FileNotFoundError:
-            try:
-                doxygen_path = (doxygen_root / doxygen_path).resolve(
-                    strict=True
-                )
-            except FileNotFoundError as err:
-                raise NotADirectoryError(
-                    "Expected doxygen to generate the folder"
-                    f" {doxygen_path} but it could not be found."
-                ) from err
-
-        self._doxygen_context = doxygen._DoxygenContext(
-            ran_doxygen=True,
-            root=doxygen_root,
-            path=doxygen_path,
-            doxyfile=Path(doxygen_file))
-        if hasattr(doxygen.setup, '_doxygen_context'):
-            doxygen.setup._doxygen_context = self._doxygen_context
-
-        if self._setup:
-            self.extensions += ["sphinx.ext.mathjax", "breathe"]
-            self.breathe_projects = {
-                self._project_name: str(self._doxygen_context.path)
-            }
-            self.breathe_default_project = self._project_name
+        self.doxygen_root = doxygen_root
+        self.doxygen_project = {
+            "name": self._project_name,
+            "path": doxygen_path,
+        }
+        self.doxyfile = doxygen_file
 
     def enable_api_reference(self) -> None:
-        """Enable embedding the doxygen generated api.
-        `run_doxygen` must be called (before or after) for this to work
-        This requires extra dependencies exposed as the api_reference option.
-        """
-        try:
-            import doxysphinx.cli
-        except ImportError:
-            raise RuntimeError("Missing optional dependencies: make sure the " 
-                               "[api_reference] feature is enabled. (e.g. "
-                               "\"pip install rocm-docs-core[api_reference]\")")
+        """Enable embedding the doxygen generated api."""
+        if not "rocm_docs.doxygen" in self.extensions:
+            self.extensions.append("rocm_docs.doxygen")
 
-        # Register the data that will be needed for doxysphinx, it will be run
-        # during setup()
-        # It cannot run just yet as we don't know the location of
-        # doxygen source and output directories.
-        # In the future rocm-docs-core could be converted to a sphinx extension,
-        # then this hack wouldn't be required.
-        doxygen.setup._doxygen_context = self._doxygen_context
+        self.doxysphinx_enabled = True
 
     def setup(self) -> None:
         """Sets up default RTD variables and copies necessary files."""
-        if self._setup:
-            return
-        self._setup = True
-
-        self.extensions = ["rocm_docs", "rocm_docs.doxygen"]
+        self.extensions.append("rocm_docs")
         self.html_title = self._project_name
-
-        if self._ran_doxygen:
-            self.extensions += ["sphinx.ext.mathjax", "breathe"]
-            self.breathe_projects = {
-                self._project_name: str(self._doxygen_context.path)
-            }
-            self.breathe_default_project = self._project_name
-        else:
-            self.breathe_projects = {}
-            self.breathe_default_project = ""
 
     def disable_main_doc_link(self):
         self.html_theme_options["link_main_doc"] = False
 
-    def copy_files(self):
-        """Insert additional files into workspace."""
-        if self._copied_files:
-            return
-        self._copied_files = True
-
-        def copy_from_package(data_dir, read_path: str, write_path: str):
-            if not data_dir.is_dir():
-                raise NotADirectoryError(
-                    f"Expected path {read_path}/{data_dir} to be traversable."
-                )
-            for entry in data_dir.iterdir():
-                entry_path: Path = self._docs_folder / write_path / entry.name
-                if entry.is_dir():
-                    entry_path.mkdir(parents=True, exist_ok=True)
-                    copy_from_package(
-                        entry,
-                        read_path + "/" + entry.name,
-                        write_path + "/" + entry.name,
-                    )
-                else:
-                    with entry.open("rb") as resource_file:
-                        # We open the resource file as a binary stream instead
-                        # of a file on disk because the latter may require
-                        # unzipping and/or the creation of a temporary file.
-                        # This is not the case when opening the file as a
-                        # stream.
-                        resource_file: BinaryIO
-                        with open(entry_path, "wb") as output_file:
-                            shutil.copyfileobj(resource_file, output_file)
-
-        pkg = importlib_resources.files("rocm_docs")
-        copy_from_package(pkg / "data", "data", ".")
 
 __all__ = ["setup", "ROCmDocs"]
--- src/rocm_docs/doxygen.py
+++ src/rocm_docs/doxygen.py
@@ -1,29 +1,159 @@
 """Doxygen sub-extension of rocm-docs-core"""
 
+import os
+import shutil
 import subprocess
 import sys
 from dataclasses import dataclass
 from pathlib import Path
+from typing import Any, BinaryIO, Dict, Optional, Tuple, Union
 
+from pydata_sphinx_theme.utils import config_provided_by_user
 from sphinx.application import Sphinx
+from sphinx.config import Config
+from sphinx.errors import ConfigError
+
+if sys.version_info < (3, 9):
+    # importlib.resources either doesn't exist or lacks the files()
+    # function, so use the PyPI version:
+    import importlib_resources
+
+else:
+    # importlib.resources has files(), so use that:
+    import importlib.resources as importlib_resources
+
+Traversable = importlib_resources.abc.Traversable
 
 
-@dataclass
-class _DoxygenContext:
-    ran_doxygen: bool = False
-    root: Path = Path()
-    path: Path = Path()
-    doxyfile: str = ""
+def _copy_files(app: Sphinx):
+    """Insert additional files into workspace."""
+
+    def copy_from_package(
+        data_dir: Traversable, read_path: str, write_path: str
+    ):
+        if not data_dir.is_dir():
+            raise NotADirectoryError(
+                f"Expected path {read_path}/{data_dir} to be traversable."
+            )
+        for entry in data_dir.iterdir():
+            entry_path: Path = Path(app.confdir) / write_path / entry.name
+            if entry.is_dir():
+                entry_path.mkdir(parents=True, exist_ok=True)
+                copy_from_package(
+                    entry,
+                    read_path + "/" + entry.name,
+                    write_path + "/" + entry.name,
+                )
+            else:
+                # We open the resource file as a binary stream instead
+                # of a file on disk because the latter may require
+                # unzipping and/or the creation of a temporary file.
+                # This is not the case when opening the file as a
+                # stream.
+                with entry.open("rb") as infile, open(entry_path, "wb") as out:
+                    shutil.copyfileobj(infile, out)
+
+    pkg = importlib_resources.files("rocm_docs")
+    copy_from_package(pkg / "data", "data", ".")
 
 
-def setup(app: Sphinx) -> None:
-    # Execute doxysphinx now that sphinx source and output directories are known
-    if not hasattr(setup, "_doxygen_context"):
+def _get_config_default(config: Config, key: str) -> Any:
+    """Get the default value for a sphinx configuration key."""
+    default_or_callable = config.values[key][0]
+    if callable(default_or_callable):
+        return default_or_callable(config)
+    else:
+        return default_or_callable
+
+
+def _run_doxygen(app: Sphinx, config: Config) -> None:
+    """Run doxygen, validating all paths."""
+
+    # To support the (legacy) ROCmDocs interface 'None' is a synonym for the
+    # default value
+    for i in ["doxygen_root", "doxyfile"]:
+        if getattr(config, i) is None:
+            setattr(config, i, _get_config_default(config, i))
+
+    doxygen_root = Path(app.confdir, config.doxygen_root)
+    try:
+        doxygen_root = doxygen_root.resolve(strict=True)
+    except FileNotFoundError as err:
+        raise ConfigError(
+            f"Expected doxygen root folder {doxygen_root} to exist"
+            " and be readable."
+        ) from err
+
+    doxyfile = Path(app.confdir, config["doxyfile"]).absolute()
+    if not doxyfile.is_file():
+        raise ConfigError(
+            f"Expected doxyfile {doxyfile} to exist and be readable."
+        )
+
+    doxygen_exe = shutil.which("doxygen")
+    if doxygen_exe is None:
+        raise RuntimeError(
+            "'doxygen' command not found! Make sure that "
+            "doxygen is installed and in the PATH"
+        )
+
+    # Running doxygen requires that the files are already copied because
+    # the Doxyfile references files distributed with rocm-docs-core
+    # (e.g. stylesheets)
+    _copy_files(app)
+    try:
+        subprocess.check_call([doxygen_exe, "--version"], cwd=doxygen_root)
+        subprocess.check_call([doxygen_exe, doxyfile], cwd=doxygen_root)
+    except subprocess.CalledProcessError as err:
+        raise RuntimeError("Failed when running doxygen") from err
+
+    _update_breathe_settings(app, doxygen_root)
+    _run_doxysphinx(app, doxygen_root, doxyfile)
+
+
+def _update_breathe_settings(app: Sphinx, doxygen_root: Path) -> None:
+    if config_provided_by_user(
+        app, "breathe_projects"
+    ) or config_provided_by_user(app, "breathe_default_project"):
         return
 
-    doxygen_context = setup._doxygen_context
-    if not doxygen_context.ran_doxygen:
-        raise RuntimeError("Doxysphinx enabled but run_doxygen not called")
+    doxygen_project: Dict = app.config.doxygen_project
+
+    # To support the (legacy) ROCmDocs interface 'None' is a synonym for the
+    # default value for each element of the Tuple
+    default: Dict = _get_config_default(app.config, "doxygen_project")
+    for key, value in doxygen_project.items():
+        if value is None:
+            doxygen_project[key] = default[key]
+
+    project_name: str = doxygen_project["name"]
+
+    # First try relative to the 
+    xml_path = Path(app.confdir, doxygen_project["path"])
+    try:
+        xml_path = xml_path.resolve(strict=True)
+    except FileNotFoundError as err:
+        raise NotADirectoryError(
+            "Expected doxygen to generate the folder"
+            f" {doxygen_path} but it could not be found."
+        ) from err
+
+    app.config.breathe_projects = {project_name: str(xml_path)}
+    app.config.breathe_default_project = project_name
+
+
+def _run_doxysphinx(app: Sphinx, doxygen_root: Path, doxyfile: Path) -> None:
+    if not app.config.doxysphinx_enabled:
+        return
+
+    try:
+        import doxysphinx.cli
+    except ImportError as err:
+        raise RuntimeError(
+            "Missing optional dependencies: make sure the "
+            "[api_reference] feature is enabled. (e.g. "
+            '"pip install rocm-docs-core[api_reference]")'
+        ) from err
 
     res = subprocess.run(
         [
@@ -33,10 +163,39 @@ def setup(app: Sphinx) -> None:
             "build",
             app.srcdir,
             app.outdir,
-            doxygen_context.doxyfile,
+            doxyfile,
         ],
-        cwd=doxygen_context.root,
+        cwd=doxygen_root,
     )
     if res.returncode != 0:
         raise RuntimeError(f"doxysphinx failed (exitcode={res.returncode:d})")
+
+
+def setup(app: Sphinx) -> Dict[str, Any]:
+    app.setup_extension("sphinx.ext.mathjax")
+    app.setup_extension("breathe")
+
+    app.add_config_value(
+        "doxygen_root", ".doxygen", rebuild="", types=[None, str, os.PathLike]
+    )
+    app.add_config_value(
+        "doxyfile",
+        lambda config: Path(config.doxygen_root, "Doxyfile"),
+        rebuild="",
+        types=[None, str, os.PathLike],
+    )
+    app.add_config_value(
+        "doxygen_project",
+        lambda config: {
+            "name": "doxygen",
+            "path": Path(config.doxygen_root, "docBin", "xml"),
+        },
+        rebuild="",
+        types=Dict[str, Union[None, str, os.PathLike]],
+    )
+    app.add_config_value("doxysphinx_enabled", False, rebuild="", types=bool)
+
+    # Should run before breathe sees their parameters, as we provide defaults.
+    app.connect("config-inited", _run_doxygen, priority=400)
+
     return {"parallel_read_safe": True, "parallel_write_safe": True}
