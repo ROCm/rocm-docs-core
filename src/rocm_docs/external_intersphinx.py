71bec4d4dca3b2031a3470f530e6ca83fe618a10
Gergely Meszaros <gergely@streamhpc.com>
2023-04-27 14:43:12 +0000

    feat(intersphinx): add version replacement in the yaml

--- src/rocm_docs/external_intersphinx.py
+++ src/rocm_docs/external_intersphinx.py
@@ -8,10 +8,13 @@ from typing import Any, Dict, List, Optional, Tuple, Union
 
 import fastjsonschema  # type: ignore[import]
 import github
+import sphinx.util.logging
 import yaml
 from pydata_sphinx_theme.utils import config_provided_by_user  # type: ignore[import]
 from sphinx.application import Sphinx
 from sphinx.config import Config
+
+import rocm_docs.util as util
 
 if sys.version_info < (3, 9):
     # importlib.resources either doesn't exist or lacks the files()
@@ -35,9 +38,11 @@ ProjectMapping = Tuple[str, Inventory]
 DEFAULT_INTERSPHINX_REPOSITORY = "RadeonOpenCompute/rocm-docs-core"
 DEFAULT_INTERSPHINX_BRANCH = "develop"
 
+logger = sphinx.util.logging.getLogger(__name__)
 
-def _parse_mapping(
-    mapping_yaml: Union[str, Traversable]
+
+def _format_mapping(
+    mapping_yaml: Union[str, Traversable], version: str
 ) -> Dict[str, ProjectMapping]:
     base = importlib_resources.files("rocm_docs") / "data"
     schema_file = base / "intersphinx_mapping.schema.json"
@@ -49,7 +54,8 @@ def _parse_mapping(
         else mapping_yaml.open(encoding="utf-8")
     )
 
-    Project = Dict[str, Union[str, None, List[Union[str, None]]]]
+    ProjectItem = Union[str, None, List[Union[str, None]]]
+    Project = Dict[str, ProjectItem]
     data: Dict[str, Union[int, Dict[str, Project]]]
     try:
         data = fastjsonschema.validate(schema, contents)
@@ -58,16 +64,20 @@ def _parse_mapping(
             str(schema_file) + ": mapping file not valid."
         ) from err
 
+    def format_item(item: Union[str, None]) -> Union[str, None]:
+        if isinstance(item, str):
+            return item.replace("${version}", version)
+        return item
+
     def get_target(project: Project) -> ProjectMapping:
         assert isinstance(project["target"], str)
-        if isinstance(project["inventory"], list):
-            return (project["target"], tuple(project["inventory"]))
+        target: str = project["target"].replace("${version}", version)
 
-        assert (
-            isinstance(project["inventory"], str)
-            or project["inventory"] is None
-        )
-        return (project["target"], project["inventory"])
+        inventory: ProjectItem = project["inventory"]
+        if isinstance(inventory, list):
+            return (target, tuple(map(format_item, inventory)))
+
+        return (target, format_item(inventory))
 
     assert isinstance(data["projects"], dict)
     return {key: get_target(value) for key, value in data["projects"].items()}
@@ -87,11 +97,12 @@ def _fetch_mapping(
 
         return contents.decoded_content.decode("utf-8")
     except github.GithubException as err:
-        print(
-            "Warning: failed to read mapping from "
-            f"{remote_repository}/{remote_filepath},"
-            f" status: {err.status}, falling back to bundled version.",
-            file=sys.stderr,
+        assert isinstance(err.data["message"], str)
+        message: str = err.data["message"]
+        logger.warning(
+            "failed to read remote mappings from "
+            f"{remote_repository}:{remote_filepath} "
+            f"on branch={remote_branch}, API returned {err.status}: {message}",
         )
         return importlib_resources.files("rocm_docs") / mapping_file_loc
 
@@ -100,13 +111,15 @@ def _update_config(app: Sphinx, _: Config) -> None:
     if not config_provided_by_user(app, "intersphinx_disabled_domains"):
         app.config.intersphinx_disabled_domains = ["std"]  # type: ignore[attr-defined]
 
-    default = _parse_mapping(
+    __, branch, ___ = util.get_branch(app.srcdir)
+    default = _format_mapping(
         _fetch_mapping(
             os.environ.get(
                 "INTERSPHINX_REPOSITORY", DEFAULT_INTERSPHINX_REPOSITORY
             ),
             os.environ.get("INTERSPHINX_BRANCH", DEFAULT_INTERSPHINX_BRANCH),
-        )
+        ),
+        branch,
     )
 
     mapping: Dict[str, Any] = app.config.intersphinx_mapping
