fa9d302214bef5419dfdfa41df53d239a07d75b5
Lauren Wrubleski <Lauren.Wrubleski@amd.com>
2023-05-17 12:39:09 -0600

    fix(renameVersionLinks.js): Wait for RTD injection

--- src/rocm_docs/rocm_docs_theme/static/renameVersionLinks.js
+++ src/rocm_docs/rocm_docs_theme/static/renameVersionLinks.js
@@ -1,4 +1,4 @@
-$(document).ready(() => {
+function renameVersionLinks() {
     $('div.rst-other-versions dl:first-child a').each( function () {
         var text = $(this).text();
         var versionRegEx = /^.*((?:[0-9]+\.){2}[0-9]+).*$/;
@@ -6,4 +6,22 @@ $(document).ready(() => {
             $(this).text(text.replaceAll(versionRegEx, '$1'));
         }
     })
+}
+
+function waitForSelector(selector, callback, backoff=100, max=15) {
+    let tries = 0;
+    const waitInterval = setInterval(() => {
+        if ($(selector).length > 0) {
+            callback()
+            clearInterval(waitInterval)
+        } else {
+            if (tries++ > max) {
+                clearInterval(waitInterval)
+            }
+        }
+    }, backoff)
+}
+
+$(document).ready(() => {
+    waitForSelector('div.rst-versions', renameVersionLinks);
 })
