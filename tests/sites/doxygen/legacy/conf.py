aa57a4383a415b109471bd5c51dc2ebfc425205b
Gergely Meszaros <gergely@streamhpc.com>
2023-10-20 12:27:09 +0000

    feat(doxygen.py): automatic setup of doxylink
    **If** doxylink is enabled by the project
    (by adding `sphinxcontrib.doxylink` to `extensions`) and `doxygen_html`
    is set, then set the settings of doxylink automatically.
    The role name is set to "doxygen", therefore references to the doxygen
    documentation in the current project can be typed as
    `{doxygen}\`entity-in-doxygen-doc\`` which could be a file, functions,
    namespace etc.
    
    This is only covers references within the same project, outside
    references will be done later.

--- tests/sites/doxygen/legacy/conf.py
+++ tests/sites/doxygen/legacy/conf.py
@@ -8,4 +8,9 @@ docs_core.enable_api_reference()
 for sphinx_var in ROCmDocs.SPHINX_VARS:
     globals()[sphinx_var] = getattr(docs_core, sphinx_var)
 
+if not "extensions" in globals():
+    extensions = []
+
+extensions += ["sphinxcontrib.doxylink"]
+
 external_projects_current_project = "a"
