cde2cb018fbd0dc8fa672bace57c1574ec461b84
Sam Wu <sam.wu2@amd.com>
2023-11-06 16:54:13 -0700

    feat(projects.py): Read header versions from link instead of hard-coding
    Relates to https://github.com/RadeonOpenCompute/rocm-docs-core/issues/270

--- src/rocm_docs/projects.py
+++ src/rocm_docs/projects.py
@@ -18,6 +18,7 @@ from pathlib import Path
 import fastjsonschema  # type: ignore[import-untyped]
 import github
 import sphinx.util.logging
+import urllib2
 import yaml
 from pydata_sphinx_theme.utils import (  # type: ignore[import-untyped]
     config_provided_by_user,
@@ -329,9 +330,13 @@ def _update_theme_configs(
     app: Sphinx, current_project: _Project | None, current_branch: str
 ) -> None:
     """Update configurations for use in theme.py"""
-    latest_version = "6.0.0"
+    latest_version = urllib2.urlopen(
+        "https://raw.githubusercontent.com/RadeonOpenCompute/rocm-docs-core/header-versions/latest_version.txt"
+    )[0]
     latest_version_string = f"docs-{latest_version}"
-    release_candidate = "6.1.0"
+    release_candidate = urllib2.urlopen(
+        "https://raw.githubusercontent.com/RadeonOpenCompute/rocm-docs-core/header-versions/release_candidate.txt"
+    )[0]
     release_candidate_string = f"docs-{release_candidate}"
 
     development_branch = _Project.default_value("development_branch")
