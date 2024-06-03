5e2bbc33f6e5ff33a51fc357ee3ae6aa303c1769
Sam Wu <22262939+samjwu@users.noreply.github.com>
2024-06-03 14:38:31 -0600

    refactor: Get versions from data branch instead of header-versions branch

--- src/rocm_docs/theme.py
+++ src/rocm_docs/theme.py
@@ -34,12 +34,12 @@ def _add_custom_context(
     doctree: object,  # noqa: ARG001
 ) -> None:
     header_latest_version = _get_version_from_url(
-        "https://raw.githubusercontent.com/ROCm/rocm-docs-core/header-versions/latest_version.txt"
+        "https://raw.githubusercontent.com/ROCm/rocm-docs-core/data/latest_version.txt"
     )
     context["header_latest_version"] = header_latest_version
 
     header_release_candidate_version = _get_version_from_url(
-        "https://raw.githubusercontent.com/ROCm/rocm-docs-core/header-versions/release_candidate.txt"
+        "https://raw.githubusercontent.com/ROCm/rocm-docs-core/data/release_candidate.txt"
     )
     context["header_release_candidate_version"] = (
         header_release_candidate_version
@@ -125,11 +125,11 @@ def _update_theme_options(app: Sphinx) -> None:
         )
 
     header_latest_version = _get_version_from_url(
-        "https://raw.githubusercontent.com/RadeonOpenCompute/rocm-docs-core/header-versions/latest_version.txt"
+        "https://raw.githubusercontent.com/RadeonOpenCompute/rocm-docs-core/data/latest_version.txt"
     )
 
     header_release_candidate_version = _get_version_from_url(
-        "https://raw.githubusercontent.com/RadeonOpenCompute/rocm-docs-core/header-versions/release_candidate.txt"
+        "https://raw.githubusercontent.com/RadeonOpenCompute/rocm-docs-core/data/release_candidate.txt"
     )
 
     default_config_opts = {
