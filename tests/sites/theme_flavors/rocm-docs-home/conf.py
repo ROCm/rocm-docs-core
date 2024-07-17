0795233ba2a1d331bb0c9c45e30cbe0501136182
Sam Wu <22262939+samjwu@users.noreply.github.com>
2024-07-17 10:25:07 -0600

    test: Add new test conf for themes

---
+++ tests/sites/theme_flavors/rocm-docs-home/conf.py
@@ -0,0 +1,16 @@
+extensions = ["rocm_docs"]
+html_theme = "rocm_docs_theme"
+html_theme_options = {"flavor": "rocm-docs-home"}
+
+external_toc_path = "./sphinx/_toc.yml"
+
+external_projects_current_project = "a"
+
+version = "1.0.0"
+release = "1.0.0"
+html_title = f"ROCm Docs Core Theme Test Project - ROCm Docs Home Flavor"
+project = "ROCm Docs Core"
+author = "Advanced Micro Devices, Inc."
+copyright = (
+    "Copyright (c) 2024 Advanced Micro Devices, Inc. All rights reserved."
+)
