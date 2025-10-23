8733c02f2a3d6ddf2772ce7aa426805d1220e986
Alex Xu <alex.xu@amd.com>
2025-10-23 17:01:23 -0400

    fix: linting

--- src/rocm_docs/theme.py
+++ src/rocm_docs/theme.py
@@ -48,9 +48,9 @@ def _parse_version(version_string: str) -> dict[str, str]:
     e.g.
     {"ROCm": "7.0.2", "AI-Developer-Hub": "v7.0", "ROCm-DS": "25.05"}
     """
+
     header_latest_version_list = [site_version.split(":") for site_version in version_string.split("\n")]
-    header_latest_version_dict = {site_version_pair[0].strip():site_version_pair[1].strip() for site_version_pair in header_latest_version_list}
-    return header_latest_version_dict
+    return {site_version_pair[0].strip():site_version_pair[1].strip() for site_version_pair in header_latest_version_list}
 
 
 def _add_custom_context(
