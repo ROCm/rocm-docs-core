53202d64a3b51b390eb67ecaade035c5d6d9d003
Peter Park <peter.park@amd.com>
2026-09-15 12:44:33 -0400

    fix(search.js): make comments a little more accurate

--- src/rocm_docs/rocm_docs_theme/static/search.js
+++ src/rocm_docs/rocm_docs_theme/static/search.js
@@ -5,15 +5,16 @@ function showRtdSearch() {
     document.dispatchEvent(new CustomEvent(RTD_SEARCH_EVENT));
 }
 
-// Trigger the Read the Docs Addons Search modal when clicking on "Search docs"
-// input from the topnav.
+// Trigger the Read the Docs Addons Search modal when focusing on the search
+// field in the left sidebar.
 const searchField = document.querySelector(SEARCH_FIELD_QUERY);
 if (searchField) {
     searchField.addEventListener("focusin", showRtdSearch);
 }
 
 // To preserve old search shortcut, open the Read the Docs Addons Search modal
-// on Ctrl+K / Cmd+K. Read the Docs Addons Search uses slash by default.
+// on Ctrl+K / Cmd+K. Read the Docs Addons Search uses slash by default and
+// doesn't need to be handled here.
 document.addEventListener("keydown", (e) => {
     if ((e.ctrlKey || e.metaKey) && e.key === "k") {
         e.preventDefault();
